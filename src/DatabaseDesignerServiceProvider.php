6acc2c0f81f77176c2062e674e2d6d13d806d913
Roberto Rielo <roberto910907@gmail.com>
2022-12-03 00:37:37 -0500

    Added new designer blade template

--- src/DatabaseDesignerServiceProvider.php
+++ src/DatabaseDesignerServiceProvider.php
@@ -4,7 +4,6 @@ namespace DBDesigner\DatabaseDesigner;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use DBDesigner\DatabaseDesigner\Commands\DatabaseDesignerCommand;
 
 class DatabaseDesignerServiceProvider extends PackageServiceProvider
 {
@@ -13,7 +12,6 @@ class DatabaseDesignerServiceProvider extends PackageServiceProvider
         $package
             ->name('laravel-database-designer')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasCommand(DatabaseDesignerCommand::class);
+            ->hasRoute('database-designer');
     }
 }
