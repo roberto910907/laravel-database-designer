f7af2bbe9fc22da234d082dacf523ac889810be8
Roberto Rielo <roberto910907@gmail.com>
2022-12-03 02:47:07 -0500

    Building the public assets with css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import '../scss/app.scss';
 
 const pinia = createPinia();
 const app = createApp(App);
